672195fa8e87641500ece3194e5279867c0c1ef7
Lushen Moodley <lushenmoodley@yahoo.com>
2024-10-20 18:20:52 +0200

    changes

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var data = new List<object>
+            {
+                new { Name = "John", Age = 30, Gender = "Male" },
+                new { Name = "Jane", Age = 25, Gender = "Female" },
+                new { Name = "Doe", Age = 35, Gender = "Male" }
+            };
+
+            // Convert data to JSON and pass it to the view
+            ViewBag.PivotData = JsonConvert.SerializeObject(data);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
